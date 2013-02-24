2d49beac28904495e449aadd073ab378e17658d5
Weihong.Guan <weihong.Guan@gmail.com>
2013-02-24 14:10:26 +0800

    add curl fun to ghosts.sh

--- ghosts.sh
+++ ghosts.sh
@@ -2,16 +2,19 @@
 
 raw=$1
 
+curl -s "http://just-ping.com/index.php?vh=$raw&s=ping%21" | grep "^xmlreqGET" | egrep -o "[^,]+," | sed "s%xmlreqGET('%http://just-ping.com/%" | tr -d "'," | xargs -n 1 curl -w "\n" -s 
+
+
 cat $raw | cut -d ";" -f 5 | sort -n | uniq | grep -e "\." > $raw.ipv4.tmp
 cat $raw | cut -d ";" -f 5 | sort -n | uniq | grep -e ":" > $raw.ipv6.tmp
 
 fping -a < $raw.ipv4.tmp > $raw.ipv4
-fping6 -a < $raw.ipv6.tmp > $eaw.ipv6
+fping6 -a < $raw.ipv6.tmp > $raw.ipv6
 
-rm $raw.ipv4.tmp $raw.ipv6.tmp
+#rm $raw.ipv4.tmp $raw.ipv6.tmp
 
 >$raw.ipv4.list
-cat $raw.ipv4 | while read line; do
+test -f $raw.ipv4 && cat $raw.ipv4 | while read line; do
 	
 	result=$(ping -c 3 -W 1 $line)
 
@@ -24,7 +27,7 @@ cat $raw.ipv4 | while read line; do
 done
 
 >$raw.ipv6.list
-cat $raw.ipv6 | while read line; do
+test -f $raw.ipv6 && cat $raw.ipv6 | while read line; do
 	
 	result=$(ping6 -c 3 -W 1 $line)
 
