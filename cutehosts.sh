2e0f47195601a5ea611c5b822d926032dff56d04
Weihong.Guan <weihong.Guan@gmail.com>
2013-02-26 17:02:36 +0800

    get iplist locally

--- cutehosts.sh
+++ cutehosts.sh
@@ -2,20 +2,24 @@
 
 raw=${1:-"google.com"}
 
->$raw
+#>$raw
 
-curl -s "http://just-ping.com/index.php?vh=$raw&s=ping%21" | egrep -o "^xmlreqGET\('[^']+" | sed "s%xmlreqGET('%http://just-ping.com/%" | while read line; do 
-	curl -m 10 -w "\n" -s $line >> $raw &
-done;
-sleep 12
+#curl -s "http://just-ping.com/index.php?vh=$raw&s=ping%21" | egrep -o "^xmlreqGET\('[^']+" | sed "s%xmlreqGET('%http://just-ping.com/%" | while read line; do 
+#	curl -m 10 -w "\n" -s $line >> $raw &
+#done;
+#sleep 12
 
-cat $raw | cut -d ";" -f 5 | sort -n | uniq | grep -e "\." | fping -a > $raw.ipv4
-cat $raw | cut -d ";" -f 5 | sort -n | uniq | grep -e ":" | fping6 -a > $raw.ipv6
+#cat $raw | cut -d ";" -f 5 | sort -n | uniq | grep -e "\." | fping -a > $raw.ipv4
+#cat $raw | cut -d ";" -f 5 | sort -n | uniq | grep -e ":" | fping6 -a > $raw.ipv6
+
+cat lists/$raw | sort -n | uniq | egrep "\." | fping -a > $raw.ipv4
+cat lists/$raw | sort -n | uniq | egrep ":" | fping6 -a > $raw.ipv6
+
 
 >$raw.ipv4.list
 test -f $raw.ipv4 && cat $raw.ipv4 | while read line; do
 	
-	result=$(ping -c 3 -W 1 $line)
+	result=$(ping -c 8 -W 1 $line)
 
 	if [ $? -eq 0 ]; then
 		rrt=$(echo $result  | sed "s%/%\n%g" | tail -n 2 | head -n 1)
@@ -27,7 +31,7 @@ done
 >$raw.ipv6.list
 test -f $raw.ipv6 && cat $raw.ipv6 | while read line; do
 	
-	result=$(ping6 -c 8 -W 3 $line)
+	result=$(ping6 -c 8 -W 1 $line)
 
 	if [ $? -eq 0 ]; then
 		rrt=$(echo $result  | sed "s%/%\n%g" | tail -n 2 | head -n 1)
